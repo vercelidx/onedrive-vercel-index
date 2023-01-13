8bebcf9b20aff839a1a1db08d7efb739fb2f3fd4
vercelidx <122579584+vercelidx@users.noreply.github.com>
2023-01-12 19:01:45 -0600

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: '4a5abe36-585a-4ed6-90c1-e016d3a3c5c9',
+  obfuscatedClientSecret: 'U2FsdGVkX18OREHukJ8nKm3PNkS6c51yiJJR16xZb2gSeeZHnYT2p1QOJDnq/FLtDcltP73NVPx+EyVO9rwpog==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
@@ -23,7 +23,7 @@ module.exports = {
   driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
 
   // The scope we require are listed here, in most cases you would not need to change this as well.
-  scope: 'user.read files.read.all offline_access',
+  scope: 'user.read files.read files.read.all offline_access',
 
   // Cache-Control header, check Vercel documentation for more details. The default settings imply:
   // - max-age=0: no cache for your browser
